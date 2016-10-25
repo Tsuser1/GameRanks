a8cf3f24a45dee491333c08faa7f201ca29ec3b2
J4D3N <jaden.buchan@hotmail.co.uk>
2016-10-25 03:26:03 +0100

    Re-Implement Event

---
+++ src/main/java/io/voidpowered/gameranks/event/PlayerRankEvent.java
@@ -0,0 +1,66 @@
+package io.voidpowered.gameranks.event;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import io.voidpowered.gameranks.api.Rank;
+
+public final class PlayerRankEvent extends Event implements Cancellable {
+
+	private static final HandlerList handlers;
+	
+	static {
+		handlers = new HandlerList();
+	}
+	
+	private final OfflinePlayer player;
+	private final Rank previousRank;
+	private Rank rank;
+	
+	private boolean cancelled;
+	
+	public PlayerRankEvent(OfflinePlayer player, Rank previousRank, Rank rank) {
+		this.player = player;
+		this.previousRank = previousRank;
+		this.rank = rank;
+	}
+	
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+
+
+	public OfflinePlayer getPlayer() {
+		return player;
+	}
+
+	public Rank getPreviousRank() {
+		return previousRank;
+	}
+
+
+	public Rank getRank() {
+		return rank;
+	}
+
+
+	public void setRank(Rank newRank) {
+		this.rank = newRank;
+	}
+	
+}
